c4c5e1e5c25c017ecb92a97dc776c796fdf94251
OlyaTru <122283583+OlyaTru@users.noreply.github.com>
2025-01-03 22:06:36 +0100

    feat: add solutions

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let result = '';
+  let k = '';
+  let arr = str.split('');
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === arr[i + 1]) {
+      k = +k + 1;
+    } else {
+      if(k) {
+        result += (k + 1) + arr[i];
+        k = '';
+      } else {
+        result += arr[i];
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = {
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,22 +2,36 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  if(!options.separator) {
+    options.separator = '+';
+  }
+  if(!options.additionSeparator) {
+    options.additionSeparator = '|';
+  }
+  if('addition' in options) {
+    options.addition += '';
+  }
+  str += '';
+  let additionArr = new Array(options.additionRepeatTimes);
+  additionArr.fill(options.addition);
+  let result = new Array(options.repeatTimes);
+  result.fill(str);
+  let mainResult = result.map((item) => item + additionArr.join(options.additionSeparator));
+  return mainResult.join(options.separator);
 }
 
 module.exports = {
